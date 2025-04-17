761d233a5bb9fb57df2e4ac35bf4e6bbb65e84ee
TengjiaoHE <hetengjiao@sjtu.edu.cn>
2025-04-17 23:03:04 +0800

    Update Exam4_HRESolver_TransSymtr_internalwaves.m

--- Exam4_HRESolver_TransSymtr_internalwaves.m
+++ Exam4_HRESolver_TransSymtr_internalwaves.m
@@ -44,29 +44,29 @@ z = 12:0.5:50; % depth sweep grid
 
 ymax     = 4000; % Maximal of transverse range
 ny       = floor(ymax/lambda*5); % Num of discretization points for transverse range
-MMadm.y  = linspace(0,ymax,ny); % Transverse range grids
-MMadm.y0 = 2000; % Source position on the y-axis
+MORparam.y  = linspace(0,ymax,ny); % Transverse range grids
+MORparam.y0 = 2000; % Source position on the y-axis
 
 xmax     = 15000; % Maximal of propagation range
 nx       = 1024; % Num of discretization points for transverse range
-MMadm.x  = linspace(0,xmax,nx); % Propagation range grids
+MORparam.x  = linspace(0,xmax,nx); % Propagation range grids
 
 % geometry of internalwaves (an ISW train)
 
 h0 = 15;
 cISW = 0.65;
 dy = 0*60*cISW:10*cISW:0*60*cISW+60*60*cISW;
-MMadm.hy = h0;
+MORparam.hy = h0;
 for j = 1:12
 
-    MMadm.hy =  MMadm.hy+10 *sech((MMadm.y-1700-25 - (j-1)*400+dy(1))/70).^2;
+    MORparam.hy =  MORparam.hy+10 *sech((MORparam.y-1700-25 - (j-1)*400+dy(1))/70).^2;
 
 end
 
 % parameters of PMLs
 
-MMadm.dpml = 5*lambda; % Thickness of PML
-MMadm.npml = 100; % Num of discretization points for PML
+MORparam.dpml = 5*lambda; % Thickness of PML
+MORparam.npml = 100; % Num of discretization points for PML
 
 
 p = 0; A = zeros(ny,nx,nz); Rj = zeros(ny,nx,2);
@@ -80,27 +80,27 @@ for ii = 1:mmax
 
     disp(['Solving modal coefficients: ' int2str(ii)  ' of ' int2str(mmax)  ' modes'] );
 
-    MMadm.kj = interp1(kh(:,1),kh(:,ii+1),MMadm.hy);
-    MMadm.kj_m = max(max(real(MMadm.kj)));
+    MORparam.kj = interp1(kh(:,1),kh(:,ii+1),MORparam.hy);
+    MORparam.kj_m = max(max(real(MORparam.kj)));
 
     % indexing the position of the source
 
-    iy0 = find(MMadm.y>=MMadm.y0,1,'first');
+    iy0 = find(MORparam.y>=MORparam.y0,1,'first');
 
 
     % mode function
 
-    vmode = interp1(phizsh(:,1),phizsh(:,ii+1),MMadm.hy);
-    vmodez = interp1(phizzsh(:,1),phizzsh(:,ii+1),MMadm.hy);
+    vmode = interp1(phizsh(:,1),phizsh(:,ii+1),MORparam.hy);
+    vmodez = interp1(phizzsh(:,1),phizzsh(:,ii+1),MORparam.hy);
     vmode_zs = vmode(iy0);
 
     % mode amplitude
 
-    [~,H,kxj,U,Nm] = TransEigSolver(f,MMadm,1);
+    [~,H,kxj,U,Nm] = TransEigSolver(f,MORparam,1);
 
-    ys = MMadm.y0+MMadm.dpml; % source depth
-    yr = linspace(MMadm.dpml,ymax+MMadm.dpml,ny); % receiver depth
-    hyr = interp1(MMadm.y,MMadm.hy,yr-MMadm.dpml);
+    ys = MORparam.y0+MORparam.dpml; % source depth
+    yr = linspace(MORparam.dpml,ymax+MORparam.dpml,ny); % receiver depth
+    hyr = interp1(MORparam.y,MORparam.hy,yr-MORparam.dpml);
 
     n = 1:length(kxj);
 
@@ -108,7 +108,7 @@ for ii = 1:mmax
     % reconstruction of Rj(x,y) using modal projection phi ---> psi
     Psi_n = sqrt(2/H)*sin(n*pi/H.*yr.');
     Phi_m = Psi_n*U./Nm.';
-    Prop = exp(1i*kxj*MMadm.x)./kxj*1i/2;
+    Prop = exp(1i*kxj*MORparam.x)./kxj*1i/2;
 
 
     Am = sqrt(2/H)*sin(n*pi/H*ys)*U;
@@ -134,7 +134,7 @@ for ii = 1:mmax
     if ii <= 5
 
         subplot(1,5,ii)
-        pcolor(yr/1000,MMadm.x/1000,mag2db(abs(Rj).'));
+        pcolor(yr/1000,MORparam.x/1000,mag2db(abs(Rj).'));
         view(2);
         shading flat;
         h_Colorbar = colorbar;
@@ -170,15 +170,15 @@ tlmax = 10 * round( tlmax / 10 );    % make sure the limits are round numbers
 tlmin = tlmax - 50;                  % min for colorbar
 
 
-x_interp = linspace(min(MMadm.x/1000),max(MMadm.x/1000),1024);
-y_interp = linspace(min(MMadm.y/1000),max(MMadm.y/1000),512);
+x_interp = linspace(min(MORparam.x/1000),max(MORparam.x/1000),1024);
+y_interp = linspace(min(MORparam.y/1000),max(MORparam.y/1000),512);
 
 
 h_fig = figure;
 set(h_fig,'position',[400 100 1000 400])
 izr = find(zr>=zs,1,'first');
 [X,Y,Z] = meshgrid(x_interp,y_interp,zr);
-P = interp2(MMadm.x/1000,MMadm.y/1000.',squeeze(tlt(:,:,izr)),x_interp,y_interp.');
+P = interp2(MORparam.x/1000,MORparam.y/1000.',squeeze(tlt(:,:,izr)),x_interp,y_interp.');
 surf(X(:,:,izr),Y(:,:,izr),Z(:,:,izr),P);
 set(gca,'ydir','reverse') ;
 view(2);
